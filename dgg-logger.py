f81dfa6a4193934be71cceea15bc8e3d5507e04a
tenacious210 <tenacious210@gmail.com>
2023-02-21 07:42:45 -0500

    Updated for dgg-bot v0.9.1

--- dgg-logger.py
+++ dgg-logger.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 from threading import Thread
 import logging
-import sys
 import os
+
 from dggbot import DGGChat, Message
 import google.cloud.storage
 import google.cloud.logging
@@ -10,12 +9,8 @@ import google.cloud.logging
 
 logging.getLogger("websocket").setLevel(logging.CRITICAL)
 logging.root.disabled = True
-logger = logging.getLogger("dgg-logger")
-logger.addHandler(logging.StreamHandler(sys.stdout))
-formatter = logging.Formatter("%(name)s: %(message)s")
-for handler in logger.handlers:
-    handler.setFormatter(formatter)
-logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class DGGLogger(DGGChat):
@@ -32,27 +27,15 @@ class DGGLogger(DGGChat):
 
     def on_msg(self, msg: Message):
         super().on_msg(msg)
-        msg_datetime = datetime.utcfromtimestamp(float(msg.timestamp / 1000))
-        msg_date = msg_datetime.strftime("%Y-%m-%d")
+        msg_date = msg.timestamp.strftime("%Y-%m-%d")
         if not self.logfile == f"{msg_date}.txt":
             if self.cloud_sync:
                 Thread(target=self.upload_logs, args=[self.logfile]).start()
             self.logfile = f"{msg_date}.txt"
-        log_time = msg_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        log_time = msg.timestamp.strftime("%Y-%m-%d %H:%M:%S")
         log = f"[{log_time} UTC] {msg.nick}: {msg.data}"
         with open(self.logfile, "a") as logfile:
             logfile.write(f"{log}\n")
-
-    def on_quit(self, msg: Message):
-        if msg.nick.lower() in self._users.keys():
-            del self._users[msg.nick.lower()]
-        for func in self._events.get("on_quit", tuple()):
-            func(msg)
-
-    def run(self, origin: str = None):
-        while True:
-            logger.info("Starting DGG logger")
-            self.ws.run_forever(origin=origin or self.URL)
 
     def upload_logs(self, log_filename: str):
         if not os.path.exists(log_filename):
@@ -66,4 +49,4 @@ class DGGLogger(DGGChat):
 
 if __name__ == "__main__":
     dgg_logger = DGGLogger()
-    dgg_logger.run()
+    dgg_logger.run_forever(sleep=0)
