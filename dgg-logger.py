b43b1876605d8064113aac23ba3a503a6eb4f48e
tenacious210 <tenacious210@gmail.com>
2023-04-13 15:10:21 -0400

    Removed DGGLogger class

--- dgg-logger.py
+++ dgg-logger.py
@@ -12,41 +12,40 @@ logging.root.disabled = True
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+chat = DGGChat()
+cloud_sync = True
 
-class DGGLogger(DGGChat):
-    cloud_sync = True
+logfile = "2000-12-31.txt"
+if cloud_sync:
+    storage_client = google.cloud.storage.Client()
+    storage_bucket = storage_client.bucket("tenadev")
+    logging_client = google.cloud.logging.Client()
+    logging_client.setup_logging()
 
-    def __init__(self):
-        super().__init__()
-        self.logfile = "2000-12-31.txt"
-        if self.cloud_sync:
-            storage_client = google.cloud.storage.Client()
-            self.storage_bucket = storage_client.bucket("tenadev")
-            logging_client = google.cloud.logging.Client()
-            logging_client.setup_logging()
 
-    def on_msg(self, msg: Message):
-        super().on_msg(msg)
-        msg_date = msg.timestamp.strftime("%Y-%m-%d")
-        if not self.logfile == f"{msg_date}.txt":
-            if self.cloud_sync:
-                Thread(target=self.upload_logs, args=[self.logfile]).start()
-            self.logfile = f"{msg_date}.txt"
-        log_time = msg.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-        log = f"[{log_time} UTC] {msg.nick}: {msg.data}"
-        with open(self.logfile, "a") as logfile:
-            logfile.write(f"{log}\n")
+def upload_logs(log_filename: str):
+    if not os.path.exists(log_filename):
+        logger.info(f"Couldn't find {log_filename} for upload")
+        return
+    blob = storage_bucket.blob(f"dgg-logs/{log_filename}")
+    blob.upload_from_filename(log_filename)
+    os.remove(log_filename)
+    logger.info(f"Uploaded {log_filename}")
 
-    def upload_logs(self, log_filename: str):
-        if not os.path.exists(log_filename):
-            logger.info(f"Couldn't find {log_filename} for upload")
-            return
-        blob = self.storage_bucket.blob(f"dgg-logs/{log_filename}")
-        blob.upload_from_filename(log_filename)
-        os.remove(log_filename)
-        logger.info(f"Uploaded {log_filename}")
+
+@chat.event()
+def on_msg(msg: Message):
+    global logfile
+    msg_date = msg.timestamp.strftime("%Y-%m-%d")
+    if not logfile == f"{msg_date}.txt":
+        if cloud_sync:
+            Thread(target=upload_logs, args=[logfile]).start()
+        logfile = f"{msg_date}.txt"
+    log_time = msg.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    log = f"[{log_time} UTC] {msg.nick}: {msg.data}"
+    with open(logfile, "a") as logfile:
+        logfile.write(f"{log}\n")
 
 
 if __name__ == "__main__":
-    dgg_logger = DGGLogger()
-    dgg_logger.run_forever(sleep=0)
+    chat.run_forever(sleep=0)
